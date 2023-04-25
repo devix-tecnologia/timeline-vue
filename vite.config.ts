28797677f05874cf950d23ba0197ec328136d40a
scart97 <scart.lucas@gmail.com>
2023-04-25 15:18:52 -0300

    build: configurando pacote

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const path = require("path");
+const { defineConfig } = require("vite");
+import vue from "@vitejs/plugin-vue";
 
+// https://github.com/quasarframework/quasar/issues/8432
 // https://vitejs.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "timeline-vue",
+      formats: ["es"],
+      fileName: (format: string) => `timeline-vue.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: { vue: "Vue" },
+      },
+    },
+    emptyOutDir: false,
+    optimizeDeps: {
+      exclude: ['vue']
+    }
+  },
+});
