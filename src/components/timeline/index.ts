2e5530c1d1b2cfeb6542f0cb62ce82788f5ef79c
scart97 <scart.lucas@gmail.com>
2023-04-25 15:17:55 -0300

    feat(components): exportando componentes principais

---
+++ src/components/timeline/index.ts
@@ -0,0 +1,20 @@
+// Moleculas
+import CabecalhoEventos from "./moleculas/CabecalhoEventos.vue";
+import DescricaoEvento from "./moleculas/DescricaoEvento.vue";
+import EventoTimeline from "./moleculas/EventoTimeline.vue";
+import HoraEvento from "./moleculas/HoraEvento.vue";
+import SeparadorPeriodo from "./moleculas/SeparadorPeriodo.vue";
+import Topo from "./moleculas/Topo.vue";
+
+// Organismos
+import LinhaDoTempo from "./organismos/LinhaDoTempo.vue";
+
+export {
+  CabecalhoEventos,
+  DescricaoEvento,
+  EventoTimeline,
+  HoraEvento,
+  SeparadorPeriodo,
+  Topo,
+  LinhaDoTempo,
+};
