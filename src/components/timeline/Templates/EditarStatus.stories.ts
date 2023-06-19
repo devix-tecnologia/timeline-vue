998b45a501c7c72c5d284e4cc7ee6f859209dcef
Fernando Gatti <contato@fernandogatti.com>
2023-06-19 16:52:45 -0300

    feat(eventoDetalhado): Alteração na listagem dos botões de status, antes com radiobutton, agora com componente de Botao e funções específicas.

--- src/components/timeline/Templates/EditarStatus.stories.ts
+++ src/components/timeline/Templates/EditarStatus.stories.ts
@@ -1,5 +1,6 @@
 import EditarStatus from "./EditarStatus.vue";
 import { Meta, StoryFn } from "@storybook/vue3";
+import { dadosEventoDetalhado } from "../EventoDetalhado.mock";
 
 export default {
   title: "Devix/Eventos/Templates/EditarStatus",
@@ -10,17 +11,20 @@ export default {
   argTypes: {},
 } as Meta<typeof EditarStatus>;
 
+const eventoUmMock = dadosEventoDetalhado[0];
+
 const Template: StoryFn<typeof EditarStatus> = (args) => ({
   components: { EditarStatus },
   setup() {
     return { args };
   },
   template:
-    '<EditarStatus :aoClicar="args.aoClicar" :salvarVisivel="args.salvarVisivel"  />',
+    '<EditarStatus :aoClicar="args.aoClicar" :salvarVisivel="args.salvarVisivel"  :dadosEvento="args.dadosEvento"  />',
 });
 
 export const Padrao = Template.bind({});
 Padrao.args = {
+  dadosEvento: eventoUmMock,
   salvarVisivel: true,
   aoClicar: (evento) => alert("Olá!"),
 };
--- src/components/timeline/Templates/EditarStatus.vue
+++ src/components/timeline/Templates/EditarStatus.vue
@@ -3,31 +3,21 @@
     <template #conteudo>
       <h2>Alterar o Status para:</h2>
 
-      <div class="botoes">
-        <label
-          class="btn realizado"
-          :class="{ selecionado: selecionado === 'realizado' }"
-        >
-          <IconeStatus class="icone" :status="`realizado`" /> realizado
-          <input type="radio" value="realizado" v-model="selecionado" />
-        </label>
-
-        <label
-          class="btn cancelado"
-          :class="{ selecionado: selecionado === 'cancelado' }"
-        >
-          <IconeStatus class="icone" :status="`cancelado`" /> cancelado
-          <input type="radio" value="cancelado" v-model="selecionado" />
-        </label>
-
-        <label
-          class="btn adiado"
-          :class="{ selecionado: selecionado === 'adiado' }"
-        >
-          <IconeStatus class="icone" :status="`adiado`" /> adiado
-          <input type="radio" value="adiado" v-model="selecionado" />
-        </label>
-      </div>
+      <BotaoStatus
+        :aparencia="getAparencia('realizado')"
+        @click="atualizarSelecionado('realizado')"
+        :status="`realizado`"
+      />
+      <BotaoStatus
+        :aparencia="getAparencia('cancelado')"
+        @click="atualizarSelecionado('cancelado')"
+        :status="`cancelado`"
+      />
+      <BotaoStatus
+        :aparencia="getAparencia('adiado')"
+        @click="atualizarSelecionado('adiado')"
+        :status="`adiado`"
+      />
     </template>
 
     <template #salvamento>
@@ -40,13 +30,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from "vue";
+import { defineComponent, PropType, ref, reactive, computed } from "vue";
 import "material-symbols/outlined.css";
 
 import { AoClicarEvento } from "../type";
 import { EventoDetalhado } from "../typeDetalhado";
 import EditarEvento from "../organismos/EditarEvento.vue";
 import IconeStatus from "../atomos/IconeStatus.vue";
+
+import BotaoStatus from "../moleculas/BotaoStatus.vue";
 
 export default defineComponent({
   props: {
@@ -57,13 +49,39 @@ export default defineComponent({
     salvarVisivel: {
       type: Boolean,
     },
+    dadosEvento: {
+      required: true,
+      type: Object as PropType<EventoDetalhado>,
+    },
   },
-  components: { EditarEvento, IconeStatus },
+  components: { EditarEvento, IconeStatus, BotaoStatus },
   setup(props) {
-    const selecionado = ref("");
+    props = reactive(props);
+
+    let selecionado = ref(props.dadosEvento.status);
+
+    function atualizarSelecionado(
+      novoValor:
+        | "atrasado"
+        | "adiantado"
+        | "adiado"
+        | "realizado"
+        | "planejado"
+        | "cancelado"
+    ) {
+      selecionado.value = novoValor;
+    }
+
+    const getAparencia = computed(() => {
+      return (botao: string) => {
+        return selecionado.value === botao ? "preenchido" : "outline";
+      };
+    });
 
     return {
       selecionado,
+      atualizarSelecionado,
+      getAparencia,
     };
   },
 });
@@ -89,88 +107,5 @@ export default defineComponent({
   text-align: center;
   width: 100%;
   color: var(--cor-acao);
-}
-
-.botoes {
-  padding-top: 2.4rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 2.4rem;
-  text-align: center;
-  width: 100%;
-}
-
-.btn {
-  text-transform: capitalize;
-  background: var(--cor-fundo);
-  padding: 1.4rem;
-  color: var(--cor-secundaria);
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid var(--cor-secundaria);
-  border-radius: 10px;
-  cursor: pointer;
-  line-height: 0;
-  font: var(--fonte);
-  width: 100%;
-  transition: all 0.1s ease-in;
-  gap: 1rem;
-}
-
-.icone {
-  font-size: 2.4rem;
-  padding: 0.4rem 1.4rem 0 0 !important;
-  display: inline-block;
-  width: auto;
-}
-
-.btn.realizado {
-  border: 1px solid var(--cor-sucesso);
-  color: var(--cor-sucesso);
-}
-
-.btn.realizado.selecionado,
-.btn.realizado:active,
-.btn.preenchido.realizado,
-.btn.realizado.selecionado .icone::before,
-.btn.realizado:active .icone::before {
-  background: var(--cor-sucesso);
-  color: var(--cor-fundo);
-}
-
-.btn.cancelado {
-  border: 1px solid var(--cor-importante);
-  color: var(--cor-importante);
-}
-.btn.cancelado.selecionado,
-.btn.cancelado:active,
-.btn.preenchido.cancelado,
-.btn.cancelado.selecionado .icone::before,
-.btn.cancelado:active .icone::before {
-  background: var(--cor-importante);
-  color: var(--cor-fundo);
-}
-
-.btn.adiado {
-  border: 1px solid var(--cor-apoio);
-  color: var(--cor-apoio);
-}
-.btn.adiado.selecionado,
-.btn.adiado:active,
-.btn.preenchido.adiado,
-.btn.adiado.selecionado .icone::before,
-.btn.adiado:active .icone::before {
-  background: var(--cor-apoio);
-  color: var(--cor-fundo);
-}
-
-input[type="radio"] {
-  position: absolute;
-  opacity: 0;
-  pointer-events: none;
 }
 </style>
