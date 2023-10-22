f40eb66bbd4d80dbe441d1cbdfe7edd06b3836cc
Sidarta Veloso <sidartaveloso@gmail.com>
2023-10-22 11:28:05 -0300

    feat: exporta componentes EventoDetalhado e EditarEvento

--- src/components/timeline/index.ts
+++ src/components/timeline/index.ts
@@ -1,8 +1,10 @@
 // Organismos
 import Timeline from "./organismos/Timeline.vue";
 import EventoTimeline from "./moleculas/EventoTimeline.vue";
+import EventoDetalhado from "./moleculas/EventoDetalhado.vue";
+import EditarEvento from "./moleculas/EditarEvento.vue";
 
 // Tipos
 import * as Tipos from "./type";
 
-export { Timeline, Tipos, EventoTimeline };
+export { Timeline, Tipos, EventoTimeline, EventoDetalhado, EditarEvento };
