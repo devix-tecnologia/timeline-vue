515ea12efcc04bbc3d48a80a5abcb1f2d86041e9
Sidarta Veloso <sidartaveloso@gmail.com>
2023-12-21 16:11:11 -0300

    build(d.ts): Configura exportação de tipos e em forma nomeada
    Eliminada exportação padrão e agora está usando apenas exportações nomeadas. Isso significa que os usuários da biblioteca Timeline-vue precisarão importar o Timeline (e quaisquer outros componentes ou funções que você exporte) especificamente pelo nome:
    
    Exemplo:
    import { Timeline, MeuComponente1, MeuComponente2 } from 'sua-biblioteca';
    
    Essa abordagem torna mais explícita a forma como os componentes e plugins são importados, e pode ajudar a evitar confusões ou problemas de compatibilidade que podem surgir com a mistura de exportações padrão e nomeadas.
    
    BREAKING CHANGE:

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { Plugin } from "vue";
+import { App, Plugin } from "vue";
 import * as components from "./components/timeline";
 import "./global.css";
 
 const Timeline: Plugin = {
-  install(app, ...options) {
-    for (const key in components) {
-      // @ts-expect-error
-      app.component(key, components[key]);
-    }
+  install(app: App, ...options: any[]) {
+    Object.keys(components).forEach((key) => {
+      const component = (components as any)[key];
+      if (component && typeof component === "object") {
+        app.component(key, component);
+      }
+    });
   },
 };
 
-export default Timeline;
+export { Timeline };
 
 export * from "./components/timeline";
