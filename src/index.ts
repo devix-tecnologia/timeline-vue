bf0060f3b1c02ad518306610df29c26d790a25d0
Fernando Gatti <contato@fernandogatti.com>
2024-10-16 14:28:51 -0300

    fix: Export plugin

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { App, Plugin } from "vue";
-import * as components from "./components/timeline";
-import "./global.css";
+import { App, Plugin } from 'vue';
+import * as components from './components/timeline';
+import './global.css';
 
 const Timeline: Plugin = {
   install(app: App, ...options: any[]) {
     Object.keys(components).forEach((key) => {
       const component = (components as any)[key];
-      if (component && typeof component === "object") {
+      if (component && typeof component === 'object') {
         app.component(key, component);
       }
     });
@@ -15,4 +15,4 @@ const Timeline: Plugin = {
 
 export { Timeline };
 
-export * from "./components/timeline";
+export * from './components/timeline';
